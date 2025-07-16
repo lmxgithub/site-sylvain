ac4594fb3a3b5c263a6abddc658b6bc46a8fb9ba
maxla <max.laurant@orange.fr>
2025-07-16 17:47:29 +0200

    Version initiale du site

---
+++ assets/js/config.js
@@ -0,0 +1,7 @@
+// Fichier de config simple (à étendre si besoin)
+const restaurant = {
+    name: "L'Épicurien",
+    horaires: "Ouvert du mardi au dimanche, de 12h à 23h",
+    telephone: "01 23 45 67 89"
+  };
+  
